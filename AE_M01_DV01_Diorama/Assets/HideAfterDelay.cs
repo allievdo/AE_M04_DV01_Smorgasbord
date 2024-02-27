a61dd151a4860b56004f0d1bb03ef2d0ef852b53
allikitten <allison.evdokimo@cox.net>
2024-02-27 11:45:50 -0700

    :)

---
+++ AE_M01_DV01_Diorama/Assets/HideAfterDelay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAfterDelay : MonoBehaviour
+{
+    public float delayInSeconds = 5f;
+    public float fadeRate = 0.25f;
+
+    private CanvasGroup canvasGroup;
+    private float startTimer;
+    private float fadeoutTimer;
+
+    private void OnEnable()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1.0f;
+
+        startTimer = Time.time + delayInSeconds;
+        fadeoutTimer = fadeRate;
+    }
+
+    private void Update()
+    {
+        //time to fade out?
+        if (Time.time >= startTimer)
+        {
+            fadeoutTimer -= Time.deltaTime;
+
+            //fadeout complete?
+            if (fadeoutTimer <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+
+            else
+            {
+                //reduce the alpha value
+                canvasGroup.alpha = fadeoutTimer / fadeRate;
+            }
+        }
+    }
+}
