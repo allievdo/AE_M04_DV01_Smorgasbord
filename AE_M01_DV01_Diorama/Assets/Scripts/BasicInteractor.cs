08675ce8a4fa6c9d64d631c409f562cd40d42027
allikitten <allison.evdokimo@cox.net>
2024-02-29 10:41:17 -0700

    :0

--- AE_M01_DV01_Diorama/Assets/Scripts/BasicInteractor.cs
+++ AE_M01_DV01_Diorama/Assets/Scripts/BasicInteractor.cs
@@ -4,6 +4,5 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class BasicInteractor : XRBaseControllerInteractor
 {
     public override void GetValidTargets(List<XRBaseInteractable> validTargets) { }
-
-    protected override List<XRBaseInteractable> ValidTargets { get; }
+    protected override List<XRBaseInteractable> validTargets { get; }
 }
